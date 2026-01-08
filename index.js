48a2c990bd1d35519cafa9cf67ea799797819a55
Khadija Daruwala <khadija.genius@gmail.com>
2026-01-08 18:59:30 -0500

    Working with ES Modules

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-console.log('Hello World!!')
-console.log(global)
-console.log(process)
+// console.log('Hello World!!')
+// console.log(global)
+// console.log(process)
 
-const {generateRandomNumber, convertCelsiusToFahrenheit} = require('./utils');
-console.log(`Random number: ${generateRandomNumber()}`);
-console.log(`Convert Celsius to Fahrenheit: ${convertCelsiusToFahrenheit(0)}`);
+// const {generateRandomNumber, convertCelsiusToFahrenheit} = require('./utils');
+// console.log(`Random number: ${generateRandomNumber()}`);
+// console.log(`Convert Celsius to Fahrenheit: ${convertCelsiusToFahrenheit(0)}`);
+
+import getPosts, {getPostsLength} from "./postController.js";
+
+console.log(getPosts())
+console.log(`Post lenght: ${getPostsLength() }`)
---
+++ postController.js
@@ -0,0 +1,11 @@
+const posts = [
+    {id: 1, title: "Post One"},
+    {id: 2, title: "Post Two"}, 
+    {id: 3, title: "Post Three"}, 
+    {id: 4, title: "Post Four"} 
+]
+
+const getPosts = () => posts;
+ 
+export const getPostsLength = () => posts.length;
+export default getPosts
