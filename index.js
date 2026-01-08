424a513f005c5e7341e18fda18160d8a1d9e865d
Khadija Daruwala <khadija.genius@gmail.com>
2026-01-08 16:46:08 -0500

    Learning to export and import with Common JS

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 console.log('Hello World!!')
 console.log(global)
 console.log(process)
+
+const {generateRandomNumber, convertCelsiusToFahrenheit} = require('./utils');
+console.log(`Random number: ${generateRandomNumber()}`);
+console.log(`Convert Celsius to Fahrenheit: ${convertCelsiusToFahrenheit(0)}`);
