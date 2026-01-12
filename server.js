cfbcdbbb44b711d00d000de9f3fa2ebfb9530211
Khadija Daruwala <khadija.genius@gmail.com>
2026-01-12 14:30:19 -0500

    Added routing

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ import http from 'http';
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-    // res.setHeader('Content-Type', 'text/html')
-    // res.statusCode = 404
-    
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.end('<h1>Hello World! This is my first Node.js server</h1>')
-
-    // res.writeHead(500, {'Content-Type': 'application/json'})
-    // res.end(JSON.stringify({message: 'Server error - 500'}))
+  
+ 
+    if (req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end('<h1>Home Page</h1>')
+    } else if (req.url === '/about'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end('<h1>About</h1>')
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'})
+        res.end('<h1>Not Found</h1>')
+    }
 })
 
 server.listen(PORT, () =>{
